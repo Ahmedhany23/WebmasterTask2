25c6f4c23cb2d461ea5aceca0de2520265843bf1
Ahmedhany23 <ahmedhany.22@hotmail.com>
2024-07-15 00:47:18 +0300

    commit on "master"

--- CalulatorApp/script.js
+++ CalulatorApp/script.js
@@ -49,16 +49,20 @@ const Result = () => {
     if (display_result.innerHTML.includes("/0")) {
       display_result.innerHTML = "Error";
       display_equation.innerHTML = "";
-    } else if (display_result.innerHTML === "=undefined") {
+    } 
+     /* If User double click eqaul with undefined result */
+    else if (display_result.innerHTML === "=undefined") {
       display_result.innerHTML = "";
-    } else {
+    } 
+    else {
       display_equation.innerHTML = display_result.innerHTML;
 
-      if (display_result.innerHTML === "Error") {
-        display_result.innerHTML = "";
-      } else {
-        display_result.innerHTML = `=${eval(display_result.innerHTML)}`;
-      }
+      /* When user start to click in the key when there is the error*/
+          if (display_result.innerHTML === "Error") {
+            display_result.innerHTML = "";
+          } else {
+            display_result.innerHTML = `=${eval(display_result.innerHTML)}`;
+          }
     }
   } catch (error) {
     display_result.innerHTML = "Error";
